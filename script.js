219e53e972892f3aab3a93f062708dfdbc09fa7f
maazfarooqui <maazaf10@gmail.com>
2025-02-21 21:44:59 +0530

    adding first version

---
+++ script.js
@@ -0,0 +1,43 @@
+let delbuttonlist = [];
+
+let calender = document.querySelector(".calender")
+let taskinput = document.querySelector(".taskInput")
+let addtask = document.querySelector(".addTask")
+
+let tasklist = document.querySelector(".taskList")
+
+
+let id =0;
+
+tasklist.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) {
+        event.target.parentElement.remove();
+    }
+});
+
+
+
+addtask.addEventListener("click", function () { 
+    let date = calender.value;
+    let task = taskinput.value;
+
+    if (!task || !date) 
+        {alert("Please enter both task and date");
+     return;}
+
+    tasklist.innerHTML += `<li class="listfromjs"> <div style="width:150px;">${task}</div>   <div> ${date}</div>  <button id=${id}  class="delete">Delete</button> </li>`;
+    let delbutton = document.getElementById(id);
+
+
+    
+    
+})
+
+
+
+
+
+
+
+
+
